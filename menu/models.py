1e461785f66b6b3e9918aa54c601b9c5269726f2
chromalemon <19nmelnichenko@chelsea-academy.org>
2024-11-20 10:30:05 +0000

    Added default navbar

--- menu/models.py
+++ menu/models.py
@@ -2,5 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-class meal(models.Model):
-    pass
+class foodItem(models.Model):
+    food_name = models.CharField(max_length=100)
+    food_price = models.DecimalField(max_digits=20, decimal_places=2)
--- menu/views.py
+++ menu/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def main(request):
-    pass
+    return render(request, "menu.html")
